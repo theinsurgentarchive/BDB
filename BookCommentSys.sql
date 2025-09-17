2cddc9764443ca237201dd23fc7ab202d9da64a6
chufam <theinsurgentarchive@gmail.com>
2025-09-16 18:25:50 -0700

    split images table into two to better represent the difference between the two sets of images

--- BookCommentSys.sql
+++ BookCommentSys.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS comments (
     book_id BIGINT NOT NULL,
     comment_id BIGINT NOT NULL AUTO_INCREMENT,
     user_id BIGINT NOT NULL,
+    image_id BIGINT NOT NULL,
     creation_date TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
     comment VARCHAR(MAX),
 --  The Average rating is going to be a Derived Attribute
@@ -10,13 +11,21 @@ CREATE TABLE IF NOT EXISTS comments (
     CONSTRAINT constrain_rating CHECK(rating BETWEEN 0 AND 5),
     FOREIGN KEY (book_id) REFERENCES books(book_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (image_id) REFERENCES user_images(image_id),
     PRIMARY KEY (comment_id)
 );
 
-CREATE TABLE IF NOT EXISTS images (
+CREATE TABLE IF NOT EXISTS user_images (
     image_id BIGINT NOT NULL AUTO_INCREMENT,
     filename VARCHAR(MAX) NOT NULL,
-    path_ VARCHAR(MAX) NOT NULL,
+    path VARCHAR(MAX) NOT NULL,
+    PRIMARY KEY (image_id)
+);
+
+CREATE TABLE IF NOT EXISTS book_images (
+    image_id BIGINT NOT NULL AUTO_INCREMENT,
+    filename VARCHAR(MAX) NOT NULL,
+    path VARCHAR(MAX) NOT NULL,
     PRIMARY KEY (image_id)
 );
 
@@ -26,16 +35,18 @@ CREATE TABLE IF NOT EXISTS books (
     summary VARCHAR(MAX),
     image_id BIGINT NOT NULL,
 --  Only allow image files to be a max size of 2048px by 2048px
-    FOREIGN KEY image_id REFERENCES images(image_id),
+    FOREIGN KEY image_id REFERENCES book_images(image_id),
     PRIMARY KEY (book_id)
 );
 
 CREATE TABLE IF NOT EXISTS users (
     user_id BIGINT NOT NULL AUTO_INCREMENT,
+    image_id BIGINT NOT NULL,
     creation_date TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
     username VARCHAR(64) UNIQUE NOT NULL,
     pass_hash VARCHAR(MAX) NOT NULL,
     salt VARCHAR(MAX) NOT NULL,
+    FOREIGN KEY image_id REFERENCES user_images(image_id),
     PRIMARY KEY (user_id)
 );
 
@@ -49,12 +60,14 @@ CREATE TABLE IF NOT EXISTS admins (
 CREATE TABLE IF NOT EXISTS forms (
     form_id BIGINT NOT NULL AUTO_INCREMENT,
     user_id BIGINT NOT NULL,
+    user_image BIGINT NOT NULL,
     book_name VARCHAR(MAX) NOT NULL,
+    book_image BIGINT NOT NULL,
     author VARCHAR(MAX) NOT NULL,
     summary VARCHAR(MAX) NOT NULL,
-    image_id BIGINT NOT NULL,
 --  Only allow image files to be a max size of 2048px by 2048px
-    FOREIGN KEY image_id REFERENCES images(image_id),
+    FOREIGN KEY book_image REFERENCES book_images(image_id),
     FOREIGN KEY user_id REFERENCES users(user_id),
+    FOREIGN KEY user_image REFERENCES user_images(image_id),
     PRIMARY KEY (form_id)
 );
