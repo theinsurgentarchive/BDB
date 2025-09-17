6b4642c71684ecc4c2e2b2c0992889e217fcd89e
chufam <theinsurgentarchive@gmail.com>
2025-09-16 17:56:15 -0700

    Updated Foreign/Primary key names to reflect ERD in BookCommentSys.sql

--- BookCommentSys.sql
+++ BookCommentSys.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS comments (
     book_id BIGINT NOT NULL,
-    inner_id BIGINT NOT NULL AUTO_INCREMENT,
+    comment_id BIGINT NOT NULL AUTO_INCREMENT,
     user_id BIGINT NOT NULL,
     creation_date TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
     comment VARCHAR(MAX),
@@ -8,45 +8,53 @@ CREATE TABLE IF NOT EXISTS comments (
 --  Average Formula: (Tallied value of ratings divided by number of ratings)
     rating TINYINT(6) NOT NULL,
     CONSTRAINT constrain_rating CHECK(rating BETWEEN 0 AND 5),
-    FOREIGN KEY (book_id) REFERENCES books(id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    FOREIGN KEY (book_id) REFERENCES books(book_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    PRIMARY KEY (comment_id)
 );
 
 CREATE TABLE IF NOT EXISTS images (
-    id BIGINT NOT NULL AUTO_INCREMENT,
+    image_id BIGINT NOT NULL AUTO_INCREMENT,
     filename VARCHAR(MAX) NOT NULL,
     path_ VARCHAR(MAX) NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (image_id)
 );
 
 CREATE TABLE IF NOT EXISTS books (
-    id BIGINT NOT NULL AUTO_INCREMENT,
+    book_id BIGINT NOT NULL AUTO_INCREMENT,
     add_date TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
     summary VARCHAR(MAX),
     image_id BIGINT NOT NULL,
 --  Only allow image files to be a max size of 2048px by 2048px
-    FOREIGN KEY image_id REFERENCES images(id),
-    PRIMARY KEY (id)
+    FOREIGN KEY image_id REFERENCES images(image_id),
+    PRIMARY KEY (book_id)
 );
 
 CREATE TABLE IF NOT EXISTS users (
-    id BIGINT NOT NULL AUTO_INCREMENT,
+    user_id BIGINT NOT NULL AUTO_INCREMENT,
     creation_date TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
     username VARCHAR(64) UNIQUE NOT NULL,
     pass_hash VARCHAR(MAX) NOT NULL,
     salt VARCHAR(MAX) NOT NULL,
-    admin BIT NOT NULL DEFAULT 0,
-    PRIMARY KEY (id)
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE IF NOT EXISTS admins (
+    admin_id BIGINT NOT NULL AUTO_INCREMENT,
+    user_id BIGINT NOT NULL,
+    FOREIGN KEY user_id REFERENCES users(user_id),
+    PRIMARY KEY (admin_id)
 );
 
 CREATE TABLE IF NOT EXISTS forms (
-    inner_id BIGINT NOT NULL AUTO_INCREMENT,
+    form_id BIGINT NOT NULL AUTO_INCREMENT,
     user_id BIGINT NOT NULL,
     book_name VARCHAR(MAX) NOT NULL,
     author VARCHAR(MAX) NOT NULL,
     summary VARCHAR(MAX) NOT NULL,
     image_id BIGINT NOT NULL,
 --  Only allow image files to be a max size of 2048px by 2048px
-    FOREIGN KEY image_id REFERENCES images(id),
-    FOREIGN KEY user_id REFERENCES users(id)
+    FOREIGN KEY image_id REFERENCES images(image_id),
+    FOREIGN KEY user_id REFERENCES users(user_id),
+    PRIMARY KEY (form_id)
 );
