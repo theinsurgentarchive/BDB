9d6988a15988ccfe3a56e00d36c4407d38491d21
chufam <theinsurgentarchive@gmail.com>
2025-09-25 13:37:09 -0700

    moved Primary key declarations in BookCommentSys.sql into column definition

--- BookCommentSys.sql
+++ BookCommentSys.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS comments (
     book_id BIGINT NOT NULL,
-    comment_id BIGINT NOT NULL AUTO_INCREMENT,
+    comment_id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     user_id BIGINT NOT NULL,
     parent_id BIGINT DEFAULT NULL,
     creation_date TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
@@ -11,44 +11,39 @@ CREATE TABLE IF NOT EXISTS comments (
     CONSTRAINT constrain_rating CHECK(rating BETWEEN 0 AND 5),
     FOREIGN KEY (book_id) REFERENCES books(book_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (parent_id) REFERENCES comments(comment_id),
-    PRIMARY KEY (comment_id)
+    FOREIGN KEY (parent_id) REFERENCES comments(comment_id)
 );
 
 CREATE TABLE IF NOT EXISTS books (
-    book_id BIGINT NOT NULL AUTO_INCREMENT,
+    book_id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     add_date TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
     summary VARCHAR(MAX),
-    image_path VARCHAR(MAX),
+    image_path VARCHAR(MAX)
 --  Only allow image files to be a max size of 2048px by 2048px
-    PRIMARY KEY (book_id)
 );
 
 CREATE TABLE IF NOT EXISTS users (
-    user_id BIGINT NOT NULL AUTO_INCREMENT,
+    user_id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     image_path VARCHAR(MAX),
     creation_date TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
     username VARCHAR(64) UNIQUE NOT NULL,
     pass_hash VARCHAR(MAX) NOT NULL,
-    salt VARCHAR(MAX) NOT NULL,
-    PRIMARY KEY (user_id)
+    salt VARCHAR(MAX) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS admins (
-    admin_id BIGINT NOT NULL AUTO_INCREMENT,
+    admin_id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     user_id BIGINT NOT NULL,
-    FOREIGN KEY user_id REFERENCES users(user_id),
-    PRIMARY KEY (admin_id)
+    FOREIGN KEY user_id REFERENCES users(user_id)
 );
 
 CREATE TABLE IF NOT EXISTS forms (
-    form_id BIGINT NOT NULL AUTO_INCREMENT,
+    form_id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     user_id BIGINT NOT NULL,
     book_name VARCHAR(MAX) NOT NULL,
     image_path VARCHAR(MAX),
     author VARCHAR(MAX) NOT NULL,
     summary VARCHAR(MAX) NOT NULL,
 --  Only allow image files to be a max size of 2048px by 2048px
-    FOREIGN KEY user_id REFERENCES users(user_id),
-    PRIMARY KEY (form_id)
+    FOREIGN KEY user_id REFERENCES users(user_id)
 );
