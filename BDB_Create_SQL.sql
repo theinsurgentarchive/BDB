383999242c71f87d3e83c219db7ca2ef9d658b33
chufam <theinsurgentarchive@gmail.com>
2025-10-01 16:07:22 -0700

    updated ERD, RM, and Create_SQL to add multiple Authors to Book/Forms

--- BDB_Create_SQL.sql
+++ BDB_Create_SQL.sql
@@ -1,55 +1,77 @@
+CREATE TABLE IF NOT EXISTS users (
+    user_id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    image_path VARCHAR(1024),
+    creation_date TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+    username VARCHAR(64) UNIQUE NOT NULL,
+    pass_hash VARCHAR(1024) NOT NULL,
+    salt VARCHAR(1024) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS admins (
+    admin_id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    user_id BIGINT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS forms (
+    form_id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    user_id BIGINT NOT NULL,
+    book_name TEXT NOT NULL,
+    image_path VARCHAR(1024),
+    author VARCHAR(256) NOT NULL,
+    summary TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS books (
+    book_id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    added TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+    published TIMESTAMP NULL DEFAULT NULL,
+    summary TEXT,
+    image_path VARCHAR(1024),
+    created_by BIGINT DEFAULT NULL,
+    FOREIGN KEY (created_by) REFERENCES forms(form_id) ON DELETE SET NULL
+);
+
 CREATE TABLE IF NOT EXISTS comments (
     book_id BIGINT NOT NULL,
     comment_id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     user_id BIGINT NOT NULL,
     parent_id BIGINT DEFAULT NULL,
     creation_date TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-    comment VARCHAR(MAX),
+    comment TEXT,
 --  The Average rating is going to be a Derived Attribute
 --  Average Formula: (Tallied value of ratings divided by number of ratings)
-    rating TINYINT(6) NOT NULL,
+    rating TINYINT NOT NULL,
     CONSTRAINT constrain_rating CHECK(rating BETWEEN 0 AND 5),
-    FOREIGN KEY (book_id) REFERENCES books(book_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (parent_id) REFERENCES comments(comment_id)
-);
-
-CREATE TABLE IF NOT EXISTS books (
-    book_id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    add_date TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-    summary VARCHAR(MAX),
-    image_path VARCHAR(MAX)
---  Only allow image files to be a max size of 2048px by 2048px
-);
-
-CREATE TABLE IF NOT EXISTS users (
-    user_id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    image_path VARCHAR(MAX),
-    creation_date TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
-    username VARCHAR(64) UNIQUE NOT NULL,
-    pass_hash VARCHAR(MAX) NOT NULL,
-    salt VARCHAR(MAX) NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS admins (
-    admin_id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    user_id BIGINT NOT NULL,
-    FOREIGN KEY user_id REFERENCES users(user_id)
-);
-
-CREATE TABLE IF NOT EXISTS forms (
-    form_id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    user_id BIGINT NOT NULL,
-    book_name VARCHAR(MAX) NOT NULL,
-    image_path VARCHAR(MAX),
-    author VARCHAR(MAX) NOT NULL,
-    summary VARCHAR(MAX) NOT NULL,
---  Only allow image files to be a max size of 2048px by 2048px
-    FOREIGN KEY user_id REFERENCES users(user_id)
+    FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (parent_id) REFERENCES comments(comment_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS genres (
-    genre VARCHAR(MAX) NOT NULL,
-    book_id BIGINT PRIMARY KEY NOT NULL,
-    FOREIGN KEY book_id REFERENCES books(book_id)
+    genre VARCHAR(256) NOT NULL,
+    book_id BIGINT NOT NULL,
+    PRIMARY KEY (genre, book_id),
+    FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS authors (
+    author_id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    name VARCHAR(156) NOT NULL
+);
+
+CREATE TABLE book_authors (
+    book_id BIGINT NOT NULL,
+    author_id BIGINT NOT NULL,
+    PRIMARY KEY (book_id, author_id),
+    FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE,
+    FOREIGN KEY (author_id) REFERENCES authors(author_id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS form_authors (
+    form_id BIGINT NOT NULL,
+    author_name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (form_id, author_name),
+    FOREIGN KEY (form_id) REFERENCES forms(form_id) ON DELETE CASCADE
 );
