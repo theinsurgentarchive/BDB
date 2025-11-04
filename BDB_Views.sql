cf71c98583673a3fb030d9e74d769bcd4f7cf3ef
acorona343 <acorona1337@gmail.com>
2025-11-04 15:54:13 -0800

    new changes to views

--- BDB_Views.sql
+++ BDB_Views.sql
@@ -1,22 +1,28 @@
+DROP VIEW IF EXISTS randbooks;
+DROP VIEW IF EXISTS randbookgenres;
+
+
+-- Homepage
+
 -- random book generator for the homepage
+CREATE OR REPLACE VIEW randbooks AS
 SELECT
   b.book_id,
-  b.name  AS title,
+  b.title  AS title,
   b.author,
   b.image_path
 FROM books b
 ORDER BY RAND()
-LIMIT 10;   
+LIMIT 10;
 
 -- top three genres by avverage ratings for the homepage
+CREATE OR REPLACE VIEW randbookgenres AS
 SELECT
   g.genre,
   ROUND(AVG(r.rating), 2) AS avg_rating
 FROM ratings AS r
-JOIN bookGenres AS bg ON bg.book_id = r.book_id
+JOIN bookgenres AS bg ON bg.book_id = r.book_id
 JOIN genres      AS g  ON g.genre    = bg.genre
 GROUP BY g.genre
 ORDER BY avg_rating DESC, g.genre
 LIMIT 5;
-
-
